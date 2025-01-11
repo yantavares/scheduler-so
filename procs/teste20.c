54bfe23aeda84b4d93cf64cbcace78823ae6fc8d
yantavares <yantdo1@gmail.com>
2025-01-11 09:56:05 -0300

    Update entrada2.txt process paths and remove timing measurements from teste*.c files

--- procs/teste20.c
+++ procs/teste20.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
-#include <time.h>
 
 int main()
 {
-    long long i;         // Variável para o loop
-    clock_t inicio, fim; // Variáveis para medir o tempo
+    long long i;
 
-    inicio = clock();
-
-    // Loop com número grande de iterações
     for (i = 0; i < 80000000000LL; i++)
         ;
 
-    // Marcar o fim do timer
-    fim = clock();
-
-    // Calcular o tempo total em segundos
-    double tempo_total = (double)(fim - inicio) / CLOCKS_PER_SEC;
-
-    // Exibir resultados
     printf("Loop finalizado. Número total de iterações: %lld\n", i);
-    printf("Tempo total do loop: %.2f segundos\n", tempo_total);
 
     return 0;
 }
